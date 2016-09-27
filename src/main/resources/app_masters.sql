00b951237f517f3652c683dfe29cb0a89714a473
Administrator <Administrator@Alex>
2016-09-28 00:54:55 +0800

    数据库备份，扩展user表的审核人和审核时间，增加实名认证certification表。

--- src/main/resources/app_masters.sql
+++ src/main/resources/app_masters.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50611
 File Encoding         : 65001
 
-Date: 2016-09-20 02:49:56
+Date: 2016-09-28 00:53:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -34,7 +34,33 @@ CREATE TABLE `tb_ads` (
 -- ----------------------------
 -- Records of tb_ads
 -- ----------------------------
-INSERT INTO `tb_ads` VALUES ('1', 'http://a5.mzstatic.com/us/r30/Purple30/v4/9d/c6/a6/9dc6a608-7d7b-a650-0d55-2c2b6375a83a/screen696x696.jpeg', 'https://www.baidu.com', '测试广告等你来点', '10', '1', null, null);
+INSERT INTO `tb_ads` VALUES ('1', 'http://od8rh27zr.bkt.clouddn.com/topic_14734389459111291210300.png', 'https://www.baidu.com', '测试广告等你来点', '11', '1', null, null);
+
+-- ----------------------------
+-- Table structure for `tb_certifications`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_certifications`;
+CREATE TABLE `tb_certifications` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `type` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `number` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `images` varchar(2000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `createdon` bigint(20) DEFAULT NULL,
+  `createdby` bigint(11) DEFAULT NULL,
+  `verifiedon` bigint(20) DEFAULT NULL,
+  `verifiedby` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of tb_certifications
+-- ----------------------------
+INSERT INTO `tb_certifications` VALUES ('33', '思科CCIE认证', 'person', '100000000001', null, 'upload/certification/person/cert_14749685472671291210300.png', '1474968566', '10000012', '111', null);
+INSERT INTO `tb_certifications` VALUES ('34', '北京瑞宁智远科技有限公司', 'company', '100000', null, 'upload/certification/company/company_cert_14749686142731291210300.png', '1474968616', '10000012', null, null);
+INSERT INTO `tb_certifications` VALUES ('35', '数据库专家', 'person', '1111', null, 'upload/certification/person/cert_14749701036851291210300.png', '1474970106', '10000012', null, null);
+INSERT INTO `tb_certifications` VALUES ('36', '111111', 'person', '11111', null, 'upload/certification/person/cert_14749708791401291210300.png', '1474970880', '10000012', null, null);
 
 -- ----------------------------
 -- Table structure for `tb_favorites`
@@ -255,7 +281,7 @@ CREATE TABLE `tb_users` (
   `nickname` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `mobile` bigint(20) DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `truename` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `realname` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `slogan` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `sex` int(1) DEFAULT NULL,
   `wechat` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -264,7 +290,8 @@ CREATE TABLE `tb_users` (
   `birthday` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `tags` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  `certification` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `authimages` varchar(2000) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `idnumber` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `vocation` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `experience` int(11) DEFAULT NULL,
   `devicetoken` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -272,37 +299,39 @@ CREATE TABLE `tb_users` (
   `registerlati` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `registerlong` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `appversion` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `verifiedon` bigint(13) DEFAULT NULL,
+  `verifiedby` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10000030 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
 -- Records of tb_users
 -- ----------------------------
-INSERT INTO `tb_users` VALUES ('10000000', '张1', '1000000', '123', 'sdfs', null, null, null, null, null, null, 'resources/avatars/avatar_23.jpg', null, null, '', null, null, null, null, null, null);
-INSERT INTO `tb_users` VALUES ('10000001', '阿斯达', '1000001', '123', 'ssssssdf', null, null, null, null, null, null, 'upload/avatar/14707479600131291210300.jpg', null, null, '', null, null, null, null, null, null);
-INSERT INTO `tb_users` VALUES ('10000002', '实施', '1000002', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_20.jpg', 'string', 'string', '', '0', 'string', '1470670476', 'string', 'string', 'string');
-INSERT INTO `tb_users` VALUES ('10000003', '水电费', '1000003', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_23.jpg', '安全,大数据,区块链', 'string', '', '0', 'string', '1470670511', 'string', 'string', 'string');
-INSERT INTO `tb_users` VALUES ('10000004', '一天', '1000004', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_12.jpg', 'string', 'string', '', '0', 'string', '1470670534', 'string', 'string', 'string');
-INSERT INTO `tb_users` VALUES ('10000005', '热VC', '1000005', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_20.jpg', 'string', 'string', '', '0', 'string', '1470670535', 'string', 'string', 'string');
-INSERT INTO `tb_users` VALUES ('10000006', '就让他', '10000006', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'string', '安全,大数据,区块链', '微软专家', '', '0', 'string', '0', 'string', 'string', 'string');
-INSERT INTO `tb_users` VALUES ('10000007', 'AAA', '1000007', '123', '1、呵呵', 'string', '0', 'string', 'string', 'string', 'string', 'upload/avatar/14714172009991291210300.png', '区块链', 'CCNA、CCNP', '数据库专家', '7', 'string', '1470670536', 'string', 'string', 'string');
-INSERT INTO `tb_users` VALUES ('10000008', '师德师风', '1000008', '123456', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_20.jpg', 'string', 'string', '', '0', 'string', '1470670536', 'string', 'string', 'string');
-INSERT INTO `tb_users` VALUES ('10000009', '谁谁谁v', '1000009', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_9.jpg', 'string', 'string', '', '0', 'string', '1470670536', 'string', 'string', 'string');
-INSERT INTO `tb_users` VALUES ('10000010', 'u大概', '1000010', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_4.jpg', 'string', 'string', '', '0', 'string', '1470670537', 'string', 'string', 'string');
-INSERT INTO `tb_users` VALUES ('10000011', '东方国际', '1000011', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_18.jpg', 'string', 'string', '', '0', 'string', '1470670537', 'string', 'string', 'string');
-INSERT INTO `tb_users` VALUES ('10000012', '我是马云', '15300086234', '111111', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'upload/avatar/14736536296271291210300.png', '区块链,DevOpt', '', '阿里巴巴创始人', '3', 'string', '1470746199', 'string', 'string', 'string');
-INSERT INTO `tb_users` VALUES ('10000014', '嚓鱼', '13888888888', '111', null, null, null, null, null, null, null, 'upload/avatar/14718411933811291210300.png', '大数据,区块链,去IOE,DevOpt', null, '助理教练', '10', null, '1470974896', null, null, null);
-INSERT INTO `tb_users` VALUES ('10000015', '未设置昵称', '13800000000', '111111', null, null, null, null, null, null, null, 'resources/avatars/avatar_25.jpg', '未设置', null, '', '1', null, '1470997461', null, null, null);
-INSERT INTO `tb_users` VALUES ('10000016', '未设置昵称', '15300086123', '111111', null, null, null, null, null, null, null, 'resources/avatars/avatar_4.jpg', '未设置', null, '', '1', null, '1470997533', null, null, null);
-INSERT INTO `tb_users` VALUES ('10000017', '未设置昵称', '15300086321', '123', null, null, null, null, null, null, null, 'resources/avatars/avatar_23.jpg', '未设置', null, '', '1', null, '1470997723', null, null, null);
-INSERT INTO `tb_users` VALUES ('10000022', '未设置昵称', '15300086789', '123456', null, null, null, null, null, null, null, 'resources/avatars/avatar_26.jpg', '未设置', null, '', '1', null, '1471251048', null, null, null);
-INSERT INTO `tb_users` VALUES ('10000023', '未设置昵称', '15300087777', '123', null, null, null, null, null, null, null, 'resources/avatars/avatar_4.jpg', '未设置', null, '', '1', null, '1471256039', null, null, null);
-INSERT INTO `tb_users` VALUES ('10000024', '未设置昵称', '15300009872', '123', null, null, null, null, null, null, null, 'resources/avatars/avatar_4.jpg', '未设置', null, '', '1', null, '1471256761', null, null, null);
-INSERT INTO `tb_users` VALUES ('10000025', '未设置昵称', '13788888888', '111', null, null, null, null, null, null, null, 'resources/avatars/avatar_6.jpg', '未设置', null, '', '1', null, '1471839390', null, null, null);
-INSERT INTO `tb_users` VALUES ('10000026', '未设置昵称', '13785017990', '1', null, null, null, null, null, null, null, 'resources/avatars/avatar_6.jpg', '未设置', null, '', '1', null, '1471850513', null, null, null);
-INSERT INTO `tb_users` VALUES ('10000027', '你好请问', '17710919923', '123456', '未设置', '未设置', null, '未设置', '未设置', '未设置', '未设置', 'resources/avatars/avatar_1.jpg', '未设置', '', '', '1', null, '1471850716', null, null, null);
-INSERT INTO `tb_users` VALUES ('10000028', '未设置昵称', '15101186029', '123456', null, null, null, null, null, null, null, 'resources/avatars/avatar_23.jpg', '未设置', null, '', '1', null, '1471850784', null, null, null);
-INSERT INTO `tb_users` VALUES ('10000029', '好人郝', '17310466497', '123456', null, null, null, null, null, null, null, 'upload/avatar/14736650924081291210300.png', '未设置', null, '', '3', null, '1471850979', null, null, null);
+INSERT INTO `tb_users` VALUES ('10000000', '张1', '1000000', '123', 'sdfs', null, null, null, null, null, null, 'resources/avatars/avatar_23.jpg', null, null, null, '', null, null, null, null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000001', '阿斯达', '1000001', '123', 'ssssssdf', null, null, null, null, null, null, 'upload/avatar/14707479600131291210300.jpg', null, null, null, '', null, null, null, null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000002', '实施', '1000002', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_20.jpg', 'string', 'string', null, '', '0', 'string', '1470670476', 'string', 'string', 'string', null, null);
+INSERT INTO `tb_users` VALUES ('10000003', '水电费', '1000003', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_23.jpg', '安全,大数据,区块链', 'string', null, '', '0', 'string', '1470670511', 'string', 'string', 'string', null, null);
+INSERT INTO `tb_users` VALUES ('10000004', '一天', '1000004', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_12.jpg', 'string', 'string', null, '', '0', 'string', '1470670534', 'string', 'string', 'string', null, null);
+INSERT INTO `tb_users` VALUES ('10000005', '热VC', '1000005', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_20.jpg', 'string', 'string', null, '', '0', 'string', '1470670535', 'string', 'string', 'string', null, null);
+INSERT INTO `tb_users` VALUES ('10000006', '就让他', '10000006', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'string', '安全,大数据,区块链', '微软专家', null, '', '0', 'string', '0', 'string', 'string', 'string', null, null);
+INSERT INTO `tb_users` VALUES ('10000007', 'AAA', '1000007', '123', '呵呵', 'string', '0', 'string', 'string', 'string', 'string', 'upload/avatar/14714172009991291210300.png', '区块链', 'CCNA、CCNP', null, '数据库专家', '7', 'string', '1470670536', 'string', 'string', 'string', null, null);
+INSERT INTO `tb_users` VALUES ('10000008', '师德师风', '1000008', '123456', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_20.jpg', 'string', 'string', null, '', '0', 'string', '1470670536', 'string', 'string', 'string', null, null);
+INSERT INTO `tb_users` VALUES ('10000009', '谁谁谁v', '1000009', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_9.jpg', 'string', 'string', null, '', '0', 'string', '1470670536', 'string', 'string', 'string', null, null);
+INSERT INTO `tb_users` VALUES ('10000010', 'u大概', '1000010', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_4.jpg', 'string', 'string', null, '', '0', 'string', '1470670537', 'string', 'string', 'string', null, null);
+INSERT INTO `tb_users` VALUES ('10000011', '东方国际', '1000011', '123', 'string', 'string', '0', 'string', 'string', 'string', 'string', 'resources/avatars/avatar_18.jpg', 'string', 'string', null, '', '0', 'string', '1470670537', 'string', 'string', 'string', null, null);
+INSERT INTO `tb_users` VALUES ('10000012', '我是马云', '15300086234', '111111', '000000', 'string', '0', 'string', 'string', 'string', 'string', 'upload/avatar/14736536296271291210300.png', '区块链,DevOpt', 'upload/certification/person/front_14749666396371291210300.png,upload/certification/person/reverse_14749666462781291210300.png,upload/certification/person/handing_14749666518861291210300.png', '130433199303153530', '阿里巴巴创始人', '3', 'string', '1470746199', 'string', 'string', 'string', null, null);
+INSERT INTO `tb_users` VALUES ('10000014', '嚓鱼', '13888888888', '111', null, null, null, null, null, null, null, 'upload/avatar/14718411933811291210300.png', '大数据,区块链,去IOE,DevOpt', null, null, '助理教练', '10', null, '1470974896', null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000015', '未设置昵称', '13800000000', '111111', null, null, null, null, null, null, null, 'resources/avatars/avatar_25.jpg', '未设置', null, null, '', '1', null, '1470997461', null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000016', '未设置昵称', '15300086123', '111111', null, null, null, null, null, null, null, 'resources/avatars/avatar_4.jpg', '未设置', null, null, '', '1', null, '1470997533', null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000017', '未设置昵称', '15300086321', '123', null, null, null, null, null, null, null, 'resources/avatars/avatar_23.jpg', '未设置', null, null, '', '1', null, '1470997723', null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000022', '未设置昵称', '15300086789', '123456', null, null, null, null, null, null, null, 'resources/avatars/avatar_26.jpg', '未设置', null, null, '', '1', null, '1471251048', null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000023', '未设置昵称', '15300087777', '123', null, null, null, null, null, null, null, 'resources/avatars/avatar_4.jpg', '未设置', null, null, '', '1', null, '1471256039', null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000024', '未设置昵称', '15300009872', '123', null, null, null, null, null, null, null, 'resources/avatars/avatar_4.jpg', '未设置', null, null, '', '1', null, '1471256761', null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000025', '未设置昵称', '13788888888', '111', null, null, null, null, null, null, null, 'resources/avatars/avatar_6.jpg', '未设置', null, null, '', '1', null, '1471839390', null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000026', '未设置昵称', '13785017990', '1', null, null, null, null, null, null, null, 'resources/avatars/avatar_6.jpg', '未设置', null, null, '', '1', null, '1471850513', null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000027', '你好请问', '17710919923', '123456', '未设置', '未设置', null, '未设置', '未设置', '未设置', '未设置', 'resources/avatars/avatar_1.jpg', '未设置', '', null, '', '1', null, '1471850716', null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000028', '未设置昵称', '15101186029', '123456', null, null, null, null, null, null, null, 'resources/avatars/avatar_23.jpg', '未设置', null, null, '', '1', null, '1471850784', null, null, null, null, null);
+INSERT INTO `tb_users` VALUES ('10000029', '好人郝', '17310466497', '123456', null, '估身价，据标签（我的身价）', null, null, null, null, null, 'upload/avatar/14736650924081291210300.png', '未设置', null, null, '', '3', null, '1471850979', null, null, null, null, null);
 
 -- ----------------------------
 -- Table structure for `tb_versions`
