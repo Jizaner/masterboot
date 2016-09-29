72250cb360bd60c69f630f5a412db36ad52a42c2
Administrator <Administrator@Alex>
2016-09-29 12:53:14 +0800

    设置context-path

--- src/main/java/com/jizan/master/service/impl/TopicServiceImpl.java
+++ src/main/java/com/jizan/master/service/impl/TopicServiceImpl.java
@@ -1,16 +1,19 @@
 package com.jizan.master.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
+
 import com.jizan.master.dao.TopicDao;
 import com.jizan.master.dao.BaseDao;
 import com.jizan.master.entity.Topic;
 import com.jizan.master.service.TopicService;
 import com.jizan.master.service.impl.BaseServiceImpl;
 import com.jizan.utils.Pager;
+import com.jizan.utils.StringUtil;
 
 @Service
 public class TopicServiceImpl extends BaseServiceImpl<Topic> implements TopicService {
@@ -41,5 +44,23 @@ public class TopicServiceImpl extends BaseServiceImpl<Topic> implements TopicSer
 	public List<Topic> pageByIds(Map map) {
 		return topicDao.pageByIds(map);
 	}
-
+	
+	public Pager pageAll(int page, int limit) {
+		Pager pager = new Pager(Integer.valueOf(page), countAll(), limit);
+		Map<String, Integer> pagerMap = new HashMap<String, Integer>();
+		pagerMap.put("startIdx", Integer.valueOf(pager.getStartIdx()));
+		pagerMap.put("limit", Integer.valueOf(pager.getLimit()));
+		List items = getBaseDao().pageBy(pagerMap);
+		for (int i = 0; i < items.size(); i++) {
+			String imagesStr=((Topic) items.get(i)).getImages();
+			String[] imagesArray=imagesStr.split(",");
+			for (int j = 0; j < imagesArray.length; j++) {
+				imagesArray[j]=imagesArray[j]+"!thumbnail";
+			}
+			String newImagesStr= StringUtil.join(imagesArray, ",");
+			((Topic) items.get(i)).setImages(newImagesStr);
+		}
+		pager.setRows(items);
+		return pager;
+	}
 }
